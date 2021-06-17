0e3446dfeed326d34b77918082d4f9842f03c589
Nelson Maldonado <play2nino@gmail.com>
2021-06-16 22:18:06 -0700

    finished

--- index.js
+++ index.js
@@ -93,11 +93,17 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model,milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank= 0;
+    this.odometer=0;
   }
-  
-  
+  Car.prototype.fill = function (gallons) {
+    return this.tank += gallons;
+  };
+
+
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
@@ -105,10 +111,16 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name,age,favoriteToy) {
+  Person.call(this,name,age);
+  this.favoriteToy=favoriteToy;
   }
  
+  Baby.prototype = Object.create(Person.prototype)
+
+  Baby.prototype.play = function(){
+    return `Playing with favorite ${this.favoriteToy}`;
+  }
   
   /* 
     TASK 4
